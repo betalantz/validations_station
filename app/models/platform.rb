e63db3b01149cccae02921d3aeda09ea9756c788
betalantz <betalantz@gmail.com>
2021-09-01 15:48:51 -0700

    adds validations to Platform

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -3,5 +3,10 @@ class Platform < ApplicationRecord
   has_many :assignments
   has_many :trains, through: :assignments
 
-  
+  validates :platform_num, numericality: { 
+      only_integer: true,
+      greater_than: 0,
+      less_than_or_equal_to: 20
+    }
+  validates :platform_num, uniqueness: { scope: :station }
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,4 +1,7 @@
 class Train < ApplicationRecord
     has_many :assignments
     has_many :platforms, through: :assignments
+
+    
+
 end
