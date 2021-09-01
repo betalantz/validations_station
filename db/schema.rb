1e856748d1f39468959f4cbb1338b22064b32a82
betalantz <betalantz@gmail.com>
2021-09-01 11:54:54 -0700

    adds seeds and schema to main

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_01_173632) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.time "arrival"
+    t.time "departure"
+    t.integer "platform_id", null: false
+    t.integer "train_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["platform_id"], name: "index_assignments_on_platform_id"
+    t.index ["train_id"], name: "index_assignments_on_train_id"
+  end
+
+  create_table "platforms", force: :cascade do |t|
+    t.string "platform_num"
+    t.integer "station_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["station_id"], name: "index_platforms_on_station_id"
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.string "train_num"
+    t.boolean "is_express"
+    t.string "origin"
+    t.string "destination"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "assignments", "platforms"
+  add_foreign_key "assignments", "trains"
+  add_foreign_key "platforms", "stations"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "🌱 Seeding stations..."
+qc = Station.create(name: "Queen's Cross", city: "Brambury")
+us = Station.create(name: "Union Station", city: "Upton Downs")
+
+puts "🌱 Seeding trains..."
+t1 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+t2 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+t3 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+t4 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+t5 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+t6 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+t7 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+t8 = Train.create(train_num: Faker::Number.decimal_part(digits: 3), is_express: Faker::Boolean.boolean, origin: Faker::Address.city, destination: Faker::Address.city)
+
+puts "🌱 Seeding platforms..."
+Station.all.each {|s|
+    Faker::Number.within(range: 4..10).times {|i|
+        Platform.create(platform_num: "#{i+1}", station: s)
+    }
+}
+
+puts "🌱 Seeding assignments..."
+p1 = Platform.first
+Assignment.create(platform: p1, train: t1, arrival: "09:11", departure: "09:18")
+Assignment.create(platform: p1, train: t2, arrival: "09:22", departure: "09:31")
+Assignment.create(platform: p1, train: t3, arrival: "09:40", departure: "09:53")
+Assignment.create(platform: p1, train: t4, arrival: "10:05", departure: "10:22")
+
+puts "✅ Done seeding!"
