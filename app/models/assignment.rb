4f13316bde271ee6fdb7aecfd6699b26ceccd968
betalantz <betalantz@gmail.com>
2021-12-16 12:07:42 -0800

    adds and removes validations and updates Gemfile

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,18 +1,23 @@
 class Assignment < ApplicationRecord
   belongs_to :platform
   belongs_to :train
-
+  
+  validate :is_valid
   validate :is_too_long
   validate :is_available
+
+  def duration
+    (self.departure - self.arrival)/60
+  end
+
+  def is_valid
+    errors.add(:arrival, "must occur before departure") if self.duration <= 0
+  end
 
   def is_too_long
     if self.duration > 20
       errors.add(:duration, "can't be longer than 20 minutes")
     end
-  end
-
-  def duration
-    (self.departure - self.arrival)/60
   end
 
   def is_available
@@ -24,3 +29,14 @@ class Assignment < ApplicationRecord
     end
   end
 end
+
+# def no_duplicate_schedules
+#   platform = Platform.find(platform_id)
+#   platform.assignments.each do |assignment|
+#     if ((arrival >= assignment.arrival && arrival <= assignment.departure ) || ( departure >= assignment.arrival  &&  departure <= assignment.departure ))
+#       errors.add(:arrival, "Trains cannot have overlapping schedules.")
+#     elsif (( assignment.arrival >= arrival  &&  assignment.arrival <= departure  ) || (  assignment.departure >= arrival  &&  assignment.departure <= departure  ))
+#       errors.add(:arrival, "Trains cannot have overlapping schedules.")
+#     end
+#   end
+# end
--- app/models/train.rb
+++ app/models/train.rb
@@ -2,7 +2,7 @@ class Train < ApplicationRecord
     has_many :assignments
     has_many :platforms, through: :assignments
 
-    validates :origin, :destination, length: { in: 3..20 }
-    validates :is_express, inclusion: [true, false]
+    validates :origin, :destination, length: { in: 3..24 }
+    # validates :is_express, inclusion: [true, false] this is redundant
 
 end
